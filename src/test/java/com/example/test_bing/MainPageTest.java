f3e46aae75eb161fa858b768dcde860ed82c4bbc
wedemeda <gagarin.rama@gmail.com>
2023-11-05 13:15:29 +0100

    Добавлен тест проверяющий переход по первой ссылке
    Также провел рефакторинг кода

--- src/test/java/com/example/test_bing/MainPageTest.java
+++ src/test/java/com/example/test_bing/MainPageTest.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import java.time.Duration;
+import java.util.List;
 
 public class MainPageTest {
     private WebDriver driver;
@@ -31,15 +32,43 @@ public class MainPageTest {
     }
 
     @Test
+    @DisplayName("Запрос Selenium остается в строке поиска после вывода результатов.")
     public void search() {
 
         String input = "Selenium";
+        String searchSelector = "#sb_form_q";
 
-        WebElement searchField = driver.findElement(By.cssSelector("#sb_form_q"));
-        searchField.sendKeys(input);
-        searchField.submit();
+        searchAction(input, searchSelector);
 
-        WebElement searchPageField = driver.findElement(By.cssSelector("#sb_form_q"));
-        assertEquals(input, searchPageField.getAttribute("value"));
+        WebElement searchPageField = driver.findElement(By.cssSelector(searchSelector));
+        assertEquals(input, searchPageField.getAttribute("value"),"Запрос Selenium отсутствует в строке поиска после вывода резултатов.");
     }
+
+    @Test
+    @DisplayName("Первая ссылка в выдаче ведет на сайт https://www.selenium.dev/")
+    public void checkUrl() {
+
+        String input = "Selenium";
+        String searchSelector = "#sb_form_q";
+
+        searchAction(input, searchSelector);
+
+        List<WebElement> results = driver.findElements(By.cssSelector(".b_attribution[tabindex='0']>cite"));
+        String searchUrl = getSearchUrl(results, 0);
+        driver.get(searchUrl);
+        assertEquals(searchUrl, driver.getCurrentUrl(),"Первая ссылка в выдаче не ведет на сайт https://www.selenium.dev/");
+
+    }
+
+        public void searchAction(String input, String searchSelector) {
+
+            WebElement searchField = driver.findElement(By.cssSelector(searchSelector));
+            searchField.sendKeys(input);
+            searchField.submit();
+        }
+
+        public String getSearchUrl(List<WebElement> results, int num) {
+
+           return results.get(num).getText()+"/";
+        }
 }
